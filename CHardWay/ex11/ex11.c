4ec06dc5abbdd564e81c86ef13013bbdd8b2d6ee
alla <a2la1101@gmail.com>
2018-01-23 15:54:37 +0200

    example 14

--- CHardWay/ex11/ex11.c
+++ CHardWay/ex11/ex11.c
@@ -5,8 +5,35 @@ int main(int argc,char *argv[]){
 	if(argc!=2){
 
 		puts("enter one Argument you stupid potato");
-		
+
 		return 1;
 	}
+
+	int i=0;
+
+	char letter;
+
+	for(i=0; argv[1][i]!='\0'; i++){
+
+		letter=argv[1][i];
+
+		switch(letter){
+
+			case 'a':
+			case 'A':
+
+				puts("found an a-A");
+			
+				break;
+			
+			default:
+
+				puts("didn't find an a-A");
+				
+		}
+
+	}
+
+	return 0;
 
 }
