545573d68f5f5145a938c46c8e692d3c7cf97a5c
alla <a2la1101@gmail.com>
2018-01-24 16:34:50 +0200

    database !

--- CHardWay/ex15/ex15.c
+++ CHardWay/ex15/ex15.c
@@ -5,16 +5,13 @@
 	difficulty ... A program to manage a Database.
 
 */
-
 #include <stdio.h>
 #include <assert.h>
 #include <stdlib.h>
 #include <errno.h>
-//
 #include <string.h>
 
 #define MAX_DATA 512
-
 #define MAX_ROWS 100
 
 struct Address{
