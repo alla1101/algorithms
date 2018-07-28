0235767d1468223a66d11f3dfe20fc946d3c6fc1
alla barakat <a2la1101@gmail.com>
2018-07-28 16:37:12 +0200

    training on previous codes in ex17 lol

--- CHardWay/ex17/ex17.c
+++ CHardWay/ex17/ex17.c
@@ -1,12 +1,39 @@
 //Exercise 19: A simple object system
 
-//Before, Do tests {ex16}
+//Before, Do tests {ex17}
 #include <stdio.h>
 #include <assert.h>
 #include <stdlib.h>
 #include <string.h>
+#include <errno.h>
 
-struct person{
-	char *name;
-	int age;
+#define MAX_DATA 512
+#define MAX_ROWS 100
+
+struct Address{
+	int id;
+	int set;
+	char name[MAX_DATA];
+	char email[MAX_DATA];
 };
+
+struct Database{
+	struct Address rows[MAX_ROWS];
+};
+
+struct Connection{
+	FILE *file;
+	struct Database *db;
+};
+
+void die(const char *message){
+	if(errno){
+		perror(message);
+	}else{
+		printf("ERROR: %s\n",message);
+	}
+	exit(1);
+}
+void Address_print(struct Address *addr){
+	printf("%d %s %s \n",addr->id,addr->name,addr->email );
+}
--- README.md
+++ README.md
@@ -1,9 +1,10 @@
 Project was made on ubuntu, linux
+
 The purpose of this project is to execute the code of different books in C.
 
 Books :
  
- "Learn C the Hard Way" you can find it in CHardWay
+ "Learn C the Hard Way" you can find it in CHardWay folder
 
 
 to compile: 
