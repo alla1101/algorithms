914cf9e0535f306ba704217526fdf3b8ec83f964
alla barakat <a2la1101@gmail.com>
2018-09-15 19:23:38 +0200

    finished exp 19

--- CHardWay/ex17/ex17.c
+++ CHardWay/ex17/ex17.c
@@ -1,94 +1,188 @@
 //Exercise 19: A simple object system
-
-//Before, Do tests {ex18}
 #include <stdio.h>
-#include <stdlib.h>
 #include <errno.h>
+#include <stdlib.h>
 #include <string.h>
+#include <time.h>
+#include "ex17.h"
 
-void die(const char *message){
-	if(errno){
-		perror(message);
-	}else{
-		printf("ERROR: %s\n",message);
-	}
-}
+int Monster_attack(void *self,int damage){
+	Monster *monster=self;
 
-typedef int (*compare_cb)(int a,int b);
-
-int *bubble_sort(int *numbers,int count, compare_cb cmp){
-	int tmp=0;
-	int i=0;
-	int j=0;
-	int *target=malloc(count * sizeof(int));
+	printf("you attack %s!\n",monster->_(description));
 	
-	if(!target){die("memory error");}
+	monster->hit_points-=damage;
 
-	memcpy(target,numbers, count*sizeof(int));
-
-	for(i=0;i<count;i++){
-		for (j = 0; j < count-1;++j)
-		{
-			if(cmp(target[j],target[j+1])>0){
-				tmp=target[j+1];
-				target[j+1]=target[j];
-				target[j]=tmp;
-			}
-		}
+	if(monster->hit_points <= 0){
+		printf("It's dead\n");
+		return 1;
 	}
 
-	return target;
+	printf("it's still alive.\n");
+	return 0;
+
 }
 
-int sorted_order(int a,int b){
-	return a-b;
+int Monster_init(void *self){
+
+	Monster *monster=self;
+
+	monster->hit_points=10;
+
+	return 1;
 }
 
-int reverse_order(int a,int b){
-	return b-a;
+Object MonsterProto={
+	.init=Monster_init,
+	.attack=Monster_attack
+};
+
+void *Room_move(void *self,Direction direction){
+	Room *room = self;
+	Room *next=NULL;
+
+	if(direction == NORTH && room->north){
+		printf("you go north, into : \n");
+		next=room->north;
+	} else if(direction == SOUTH && room->south){
+		printf("you go south, into : \n");
+		next=room->south;
+	} else if(direction == EAST && room->east){
+		printf("you go east, into : \n");
+		next=room->east;
+	} else if(direction == WEST && room->west){
+		printf("you go west, into : \n");
+		next=room->west;
+	} else {
+		printf("you can't go that direction.\n");
+		next=NULL;
+	}
+
+	if(next){
+		next->_(describe)(next);
+	}
+
+	return next;
 }
-int strange_order(int a,int b){
-	if(a==0||b==0){
-		return 0;
+
+int Room_attack(void *self, int damage){
+	Room *room=self;
+	Monster *monster=room->bad_guy;
+
+	if(!monster){
+		printf("you flail in the air at nothing. Idiot. \n");
 	}else{
-		return a%b;
+
+		monster->_(attack)(monster,damage);
 	}
+
+	return 1;
 }
 
-void test_sorting(int *numbers,int count,compare_cb cmp){
-	int i=0;
-	int *sorted=bubble_sort(numbers,count,cmp);
+Object RoomProto={
+	.move=Room_move,
+	.attack=Room_attack
+};
 
-	if(!sorted){die("failed to sort as requested");}
+void *Map_move(void *self,Direction direction){
+	Map *map=self;
+	Room *location = map->location;
+	Room *next = NULL;
 
-	for(i=0;i<count;i++){
-		printf("%d ",sorted[i]);
+	next=location->_(move)(location,direction);
+	if(next){
+		map->location=next;
 	}
-	printf("\n");
 
-	free(sorted);
+	return next;
+}
+
+int Map_attack(void *self,int damage){
+	Map *map=self;
+	Room *location=map->location;
+	return location->_(attack)(location,damage);
+}
+int Map_init(void *self){
+	Map *map=self;
+
+	//make some rooms for small map
+	Room *hall=NEW(Room,"the great Hall");
+	Room *throne=NEW(Room,"the great throne");
+	Room *arena=NEW(Room,"the great arena");
+	Room *kitchen=NEW(Room,"the great kitchen");
+
+	arena->bad_guy=NEW(Monster,"the evil mintaur");
+
+	//setup map rooms
+	hall->north=throne;
+	
+	throne->west=arena;
+	throne->east=kitchen;
+	throne->south=hall;
+
+	arena->east=throne;
+	kitchen->west=throne;
+
+	//start the map and the character off in the hall
+	map->start=hall;
+	map->location=hall;
+	return 1;
+}
+
+Object MapProto={
+	.init=Map_init,
+	.move=Map_move,
+	.attack=Map_attack
+};
+
+int process_input(Map *game){
+	printf("\n> ");
+	char ch=getchar();
+	getchar(); // eat enter
+	int damage=rand()%4;
+
+	switch(ch){
+		case -1:
+			printf("giving up? you sucker .\n");
+			return 0;
+			break;
+		case 'n':
+			game->_(move)(game,NORTH);
+			break;
+		case 's':
+			game->_(move)(game,SOUTH);
+			break;
+		case 'e':
+			game->_(move)(game,EAST);
+			break;
+		case 'w':
+			game->_(move)(game,WEST);
+			break;
+		case 'a':
+			game->_(attack)(game,damage);
+			break;
+		case 'l':
+			printf("you can go: \n");
+			if(game->location->north)printf("north\n");
+			if(game->location->south)printf("south\n");
+			if(game->location->west)printf("west\n");
+			if(game->location->east)printf("east\n");
+			break;
+		default:
+			printf("what? %d\n",ch);
+	}
+
+	return 1;
 }
 
 int main(int argc,char *argv[]){
-	if(argc<2) die("Usage:./ex17 4 3 1 5 6");
+	//simple way to setup randomness
+	srand(time(NULL));
 
-	int count=argc-1;
-
-	int i=0;
-	char **inputs=argv+1;
-
-	int *numbers=malloc(count*sizeof(int));
-	if(!numbers){die("memory Error");}
-
-	for(i=0;i<count;i++){
-		numbers[i]=atoi(inputs[i]);
-	}
-
-	test_sorting(numbers,count,sorted_order);
-	test_sorting(numbers,count,reverse_order);
-	test_sorting(numbers,count,strange_order);
-
-	free(numbers);
-
-	return 0;
+	//Make our map to work with
+	Map *game=NEW(Map,"the hall of the minotaur.\n");
+	printf("you enter the ");
+	game->location->_(describe)(game->location);
+	while(process_input(game)){}
+		return 0;
 }
